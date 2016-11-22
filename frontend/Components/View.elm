8bee69f7d9fc8a90b38ed6808887aa3c2b8718e2
Bendik Solheim <bendik@solheim.im>
2016-11-22 20:49:27 +0100

    Group components by environment

--- frontend/Components/View.elm
+++ frontend/Components/View.elm
@@ -4,20 +4,38 @@ import Components.Model exposing (..)
 import Components.Messages exposing (..)
 import Component.Model
 import Component.View
-import Html exposing (Html, div)
+import Html exposing (Html, div, text)
 import Html.Attributes exposing (class)
 import List
+import String exposing (toUpper, left, dropLeft)
 
 
 view : Model -> Html Msg
 view model =
     let
         components =
-            List.map viewComponent model.components
+            List.map viewEnvironment model.environments
     in
         div [ class "components" ] components
+
+
+viewEnvironment : Components.Model.Environment -> Html Msg
+viewEnvironment environment =
+    let
+        components =
+            List.map viewComponent environment.components
+    in
+        div [ class "components__environment" ]
+            [ div [ class "components__environment-name" ] [ text <| capitalize environment.name ]
+            , div [ class "components__environment-components" ] components
+            ]
 
 
 viewComponent : Component.Model.Model -> Html Msg
 viewComponent model =
     Html.map ComponentMsg (Component.View.view model)
+
+
+capitalize : String -> String
+capitalize s =
+    toUpper (left 1 s) ++ dropLeft 1 s

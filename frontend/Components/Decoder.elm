583caf8c27ce11ce49515023ef3dbce27affed0f
Bendik Solheim <bendik@solheim.im>
2016-11-22 20:49:04 +0100

    Add LOTS of function just to get groupWhile. So sorry.

--- frontend/Components/Decoder.elm
+++ frontend/Components/Decoder.elm
@@ -14,3 +14,61 @@ decoder =
 decodeModel : List Component.Model.Model -> Decoder Model
 decodeModel componentList =
     succeed (Model componentList)
+partitionEnv : List Component.Model.Model -> Environment
+partitionEnv ls =
+    case List.head ls of
+        Just component ->
+            Environment component.environment ls
+
+        Nothing ->
+            Environment "Unknown" ls
+
+
+groupWhile : (a -> a -> Bool) -> List a -> List (List a)
+groupWhile fn ls =
+    case ls of
+        [] ->
+            []
+
+        x :: xs ->
+            let
+                ( ys, zs ) =
+                    span (fn x) xs
+            in
+                (x :: ys) :: groupWhile fn zs
+
+
+span : (a -> Bool) -> List a -> ( List a, List a )
+span p xs =
+    ( takeWhile p xs, dropWhile p xs )
+
+
+env : Component.Model.Model -> Component.Model.Model -> Bool
+env a b =
+    a.environment == b.environment
+
+
+takeWhile : (a -> Bool) -> List a -> List a
+takeWhile predicate list =
+    case list of
+        [] ->
+            []
+
+        x :: xs ->
+            if (predicate x) then
+                x :: takeWhile predicate xs
+            else
+                []
+
+
+dropWhile : (a -> Bool) -> List a -> List a
+dropWhile predicate list =
+    case list of
+        [] ->
+            []
+
+        x :: xs ->
+            if (predicate x) then
+                dropWhile predicate xs
+            else
+                list

51aec819a3e0a80f6bcb1eb88f1594b97a4c2d7a
Espen Herseth Halvorsen <espen.halvorsen@bekk.no>
2016-11-29 20:15:17 +0100

    Update deploy script to use AWS profiles

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -4,12 +4,10 @@ GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
 echo -e "${GREEN}Fetching your active Elastic Beanstalk environments...${NC}"
-eb list
+eb list --profile panopticon
 echo ""
 echo -e "${GREEN}Type name of the environment you want to deploy to:${NC}"
 read ENVIRONMENT
 echo -e "${GREEN}Deploying backend to $ENVIRONMENT${NC}"
 
-eb use "$ENVIRONMENT"
-
-eb deploy
+eb deploy "$ENVIRONMENT" --profile panopticon

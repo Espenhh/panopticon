6b99e134d95f13b1dfe20f9e8e06eacd6e88d038
Espen Herseth Halvorsen <espen.halvorsen@bekk.no>
2016-11-29 14:47:21 +0100

    Possible to deploy backend to any active environments

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,3 +1,15 @@
 #/bin/bash
 
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+echo -e "${GREEN}Fetching your active Elastic Beanstalk environments...${NC}"
+eb list
+echo ""
+echo -e "${GREEN}Type name of the environment you want to deploy to:${NC}"
+read ENVIRONMENT
+echo -e "${GREEN}Deploying backend to $ENVIRONMENT${NC}"
+
+eb use "$ENVIRONMENT"
+
 eb deploy

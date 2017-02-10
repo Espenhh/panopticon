decab2f7ef523efabcb376fefbf86e62613a2af0
Espen Herseth Halvorsen <espen.halvorsen@bekk.no>
2017-02-11 00:29:47 +0100

    Report missing instances to slack

---
+++ backend/src/main/java/no/panopticon/schedulers/MissingRunningUnitsChecker.java
@@ -0,0 +1,58 @@
+package no.panopticon.schedulers;
+
+import no.panopticon.integrations.slack.SlackClient;
+import no.panopticon.storage.RunningUnit;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.*;
+
+import static java.util.stream.Collectors.toList;
+
+
+@Service
+public class MissingRunningUnitsChecker {
+
+    private final Logger LOG = LoggerFactory.getLogger(this.getClass());
+    private final SlackClient slackClient;
+
+    private ConcurrentMap<RunningUnit, LocalDateTime> checkins = new ConcurrentHashMap<>();
+
+    private Set<RunningUnit> alertedAbout = new HashSet<>();
+
+    private static final ScheduledExecutorService SCHEDULER = Executors.newScheduledThreadPool(1);
+
+    @Autowired
+    public MissingRunningUnitsChecker(SlackClient slackClient) {
+        this.slackClient = slackClient;
+        SCHEDULER.scheduleAtFixedRate(this::checkForMissingRunningUnits, 0, 1, TimeUnit.MINUTES);
+    }
+
+    private void checkForMissingRunningUnits() {
+        checkins.entrySet().forEach(e -> {
+            if (e.getValue().isBefore(LocalDateTime.now().minus(5, ChronoUnit.MINUTES)) && !alertedAbout.contains(e.getKey())) {
+                alertedAbout.add(e.getKey());
+                slackClient.indicateMissingRunningUnit(e.getKey());
+            }
+        });
+        List<RunningUnit> reappeared = alertedAbout.stream()
+                .filter(runningUnit -> {
+                    LocalDateTime lastSeen = checkins.get(runningUnit);
+                    return lastSeen != null && lastSeen.isAfter(LocalDateTime.now().minus(5, ChronoUnit.MINUTES));
+                })
+                .peek(slackClient::indicateReturnedRunningUnit)
+                .collect(toList());
+        alertedAbout.removeAll(reappeared);
+    }
+
+    public void checkin(RunningUnit unit) {
+        checkins.put(unit, LocalDateTime.now());
+    }
+}

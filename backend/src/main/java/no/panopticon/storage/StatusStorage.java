54883a6a3ca07a91855817b7da1d2ba11de51ad5
Espen Herseth Halvorsen <espen.halvorsen@bekk.no>
2017-02-11 01:21:54 +0100

    Remove old stuff from the view

--- backend/src/main/java/no/panopticon/storage/StatusStorage.java
+++ backend/src/main/java/no/panopticon/storage/StatusStorage.java
@@ -10,10 +10,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.temporal.ChronoUnit;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.*;
 
 import static java.util.stream.Collectors.toList;
 
@@ -27,10 +28,26 @@ public class StatusStorage {
 
     private ConcurrentMap<RunningUnit, StatusSnapshot> currentStatuses = new ConcurrentHashMap<>();
 
+    private static final ScheduledExecutorService SCHEDULER = Executors.newScheduledThreadPool(1);
+
     @Autowired
     public StatusStorage(MissingRunningUnitsAlerter missingRunningUnitsAlerter, StatusAlerter statusAlerter) {
         this.missingRunningUnitsAlerter = missingRunningUnitsAlerter;
         this.statusAlerter = statusAlerter;
+
+        SCHEDULER.scheduleAtFixedRate(this::removeOldRunningUnits, 0, 1, TimeUnit.MINUTES);
+    }
+
+    private void removeOldRunningUnits() {
+        List<RunningUnit> toRemove = currentStatuses.entrySet().stream()
+                .filter(e -> e.getValue().isOlderThan(30, ChronoUnit.MINUTES))
+                .map(Map.Entry::getKey)
+                .collect(toList());
+
+        toRemove.forEach(r -> {
+            LOG.info("Removing old missing instance: " + r);
+            currentStatuses.remove(r);
+        });
     }
 
     public void processUpdatedStatus(UpdatedStatus updatedStatus) {

5d98e9187816264795b0022c6bdec685de38e553
Espen Herseth Halvorsen <espen.halvorsen@bekk.no>
2017-02-11 01:03:26 +0100

    Ignore manual test

--- backend/src/test/java/no/panopticon/integrations/slack/SlackClientManualTest.java
+++ backend/src/test/java/no/panopticon/integrations/slack/SlackClientManualTest.java
@@ -3,8 +3,10 @@ package no.panopticon.integrations.slack;
 import no.panopticon.config.SlackConfiguration;
 import no.panopticon.storage.RunningUnit;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
+@Ignore
 public class SlackClientManualTest {
 
     private SlackClient slack;

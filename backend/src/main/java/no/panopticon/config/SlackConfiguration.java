6b46d357deb1983993072dda07371e733bb965bf
Espen Herseth Halvorsen <espen.halvorsen@bekk.no>
2017-02-11 01:32:16 +0100

    Add panopticon URL to config

--- backend/src/main/java/no/panopticon/config/SlackConfiguration.java
+++ backend/src/main/java/no/panopticon/config/SlackConfiguration.java
@@ -11,11 +11,17 @@ public class SlackConfiguration {
 
     public String channel;
 
+    public String panopticonurl;
+
     public void setToken(String token) {
         this.token = token;
     }
 
     public void setChannel(String channel) {
         this.channel = channel;
+    }
+
+    public void setPanopticonurl(String panopticonurl) {
+        this.panopticonurl = panopticonurl;
     }
 }

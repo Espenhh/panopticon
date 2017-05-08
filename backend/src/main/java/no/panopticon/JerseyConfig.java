ec40311ea07ec2f37509ffee6bf5088cf688dcfb
Bendik Solheim <bendik@solheim.im>
2017-05-08 16:19:18 +0200

    Register Auth0 filter

--- backend/src/main/java/no/panopticon/JerseyConfig.java
+++ backend/src/main/java/no/panopticon/JerseyConfig.java
@@ -2,6 +2,7 @@ package no.panopticon;
 
 import no.panopticon.api.external.ExternalStatusResource;
 import no.panopticon.api.internal.InternalStatusResource;
+import no.panopticon.filter.AuthenticatedWithAuth0Filter;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.stereotype.Component;
 
@@ -11,6 +12,7 @@ public class JerseyConfig extends ResourceConfig {
     public JerseyConfig() {
         register(ExternalStatusResource.class);
         register(InternalStatusResource.class);
+        register(AuthenticatedWithAuth0Filter.class);
     }
 
 }

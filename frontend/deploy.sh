ad1f786c1803765c92acb00f4157350357b08670
Håkon Ødegård Løvdal <post@hakloev.no>
2018-07-24 08:40:59 +0200

    Update deploy script

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -9,7 +9,7 @@ NC='\033[0m' # No Color
 REGION="eu-central-1"
 
 echo -e "${GREEN}Fetching your active buckets in region $REGION...${NC}"
-aws s3 ls --profile panopticon | grep panopticon
+aws s3 ls | grep panopticon
 echo ""
 echo -e "${GREEN}Type bucket name where you want to deploy panoopticon frontend:${NC}"
 read BUCKET_NAME
@@ -17,4 +17,4 @@ echo -e "${GREEN}Deploying frontend to $BUCKET_NAME in $REGION${NC}"
 
 npm run build
 
-aws s3 sync ./dist/ "s3://$BUCKET_NAME" --region="$REGION" --profile panopticon --delete
+aws s3 sync ./dist/ "s3://$BUCKET_NAME" --region="$REGION" --delete

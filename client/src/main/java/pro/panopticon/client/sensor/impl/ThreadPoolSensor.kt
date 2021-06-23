f228fc797b514314cc891bd954d902277c1b4a69
Knut Erik Langdahl <57355800+knutbekk@users.noreply.github.com>
2021-06-23 17:45:37 +0200

    ✨ Generell sensor for å overvåke threadpools (#19)

---
+++ client/src/main/java/pro/panopticon/client/sensor/impl/ThreadPoolSensor.kt
@@ -0,0 +1,37 @@
+package pro.panopticon.client.sensor.impl
+
+import com.amazonaws.services.cloudwatch.model.StandardUnit
+import pro.panopticon.client.model.Measurement
+import pro.panopticon.client.sensor.Sensor
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.ThreadPoolExecutor
+
+class ThreadPoolSensor : Sensor {
+    private val threadPools = ConcurrentHashMap<String, ThreadPoolExecutor>()
+
+    fun register(key: String, threadPool: ThreadPoolExecutor) {
+        threadPools[key] = threadPool
+    }
+
+    override fun measure(): List<Measurement> {
+        return threadPools.entries.flatMap { (key, threadPool) ->
+            mutableListOf(
+                createMeasurement("$key.threadpool.poolsize", threadPool.poolSize),
+                createMeasurement("$key.threadpool.largestpoolsize", threadPool.largestPoolSize),
+                createMeasurement("$key.threadpool.activecount", threadPool.activeCount),
+                createMeasurement("$key.threadpool.taskcount", threadPool.taskCount),
+                createMeasurement("$key.threadpool.completedcount", threadPool.completedTaskCount),
+            )
+        }
+    }
+
+    private fun createMeasurement(key: String, value: Number): Measurement {
+        return Measurement(
+            key,
+            "INFO",
+            value.toString(),
+            Measurement.CloudwatchValue(value.toDouble(), StandardUnit.Count),
+            ""
+        )
+    }
+}
